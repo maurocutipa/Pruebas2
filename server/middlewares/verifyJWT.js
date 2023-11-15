df9998291a5a8943a09e7dabcaac05ec8e39819f
Mayko2003 <maykox34f@gmail.com>
2023-11-15 17:06:09 -0300

    chore: add auth

---
+++ server/middlewares/verifyJWT.js
@@ -0,0 +1,20 @@
+const jwt = require('jsonwebtoken')
+
+const verifyJWT = (req, res, next) => {
+    const authHeader = req.cookies
+    if (!authHeader?.jwt) return res.sendStatus(401);
+    const token = authHeader.jwt;
+    jwt.verify(
+        token,
+        process.env.JWT_SECRET,
+        (err, decoded) => {
+            if (err) return res.sendStatus(403); //token inválido
+            req.usuario = decoded.usuario;
+            //req.roles = decoded.roles,
+            req.idUsuario = decoded.idUsuario
+            next();
+        }
+    );
+}
+
+module.exports = verifyJWT
