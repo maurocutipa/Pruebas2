a45ea9bcd245079caa3e4f91bdf10b509c7ef7b6
mauromamani <mauromamani.b@gmail.com>
2023-11-26 16:10:48 -0300

    cargar firma

--- client/src/components/pages/BandejaDenuncias/RatificarDenuncia/CampoFirma.jsx
+++ client/src/components/pages/BandejaDenuncias/RatificarDenuncia/CampoFirma.jsx
@@ -1,48 +1,100 @@
 /* eslint-disable react/prop-types */
 import { Button } from 'primereact/button';
 import SignaturePad from 'react-signature-canvas';
+import { useAppDispatch } from '@/store/hooks';
+import {
+  setFirmaDenunciante,
+  setFirmaFuncionario,
+  clearFirmaDenunciante,
+  clearFirmaFuncionario,
+} from '@/store/denuncias/ratificarDenuncia/ratificarDenuncia.slice';
+import { Dialog } from 'primereact/dialog';
 
-export const CampoFirma = ({ title, setSignature, signatureRef }) => {
+export const CampoFirma = ({
+  id,
+  type,
+  title,
+  signatureRef,
+  visible,
+  setVisible,
+}) => {
+  const dispatch = useAppDispatch();
+
   const handleSave = () => {
     if (signatureRef.current.isEmpty()) {
       return;
     }
 
-    setSignature(
-      setSignature.current.getTrimmedCanvas().toDataURL('image/png')
-    );
+    const image = signatureRef.current
+      .getTrimmedCanvas()
+      .toDataURL('image/png');
+
+    if (type === 'denunciante') {
+      dispatch(setFirmaDenunciante(image));
+    }
+
+    if (type === 'funcionario') {
+      dispatch(setFirmaFuncionario(image));
+    }
+
+    onHide();
+  };
+
+  const handleClear = () => {
+    if (type === 'denunciante') {
+      dispatch(clearFirmaDenunciante());
+    }
+
+    if (type === 'funcionario') {
+      dispatch(clearFirmaFuncionario());
+    }
+
+    signatureRef.current.clear();
+    onHide();
+  };
+
+  const onHide = () => {
+    setVisible(false);
   };
 
   return (
-    <section className='mx-2'>
-      <div className='mb-1'>
-        <h3>{title}</h3>
+    <Dialog
+      draggable={false}
+      header={`${title} - Denuncia N° ${id}`}
+      visible={visible}
+      onHide={onHide}
+      className='md:w-6 w-8'
+    >
+      <section className='mx-2'>
+        <div className='mb-1'>
+          <h3>Ingrese Firma y DNI</h3>
 
-        <div className='mt-2'>
-          <SignaturePad
-            ref={signatureRef}
-            canvasProps={{ className: 'signature-canvas' }}
-          />
-
-          <div className='mt-3'>
-            <Button
-              size='small'
-              label='Guardar'
-              className='btn-blue-mpa mr-2'
-              type='button'
-              onClick={handleSave}
+          <div className='mt-2'>
+            <SignaturePad
+              ref={signatureRef}
+              canvasProps={{ className: 'signature-canvas' }}
             />
 
-            <Button
-              size='small'
-              label='Limpiar'
-              className='bg-bluegray-100 text-gray-900 border-bluegray-100 hover:bg-bluegray-200'
-              type='button'
-              onClick={() => signatureRef.current.clear()}
-            />
+            <div className='mt-3 text-right'>
+              <Button
+                size='small'
+                label='Guardar'
+                className='btn-blue-mpa mr-2'
+                type='button'
+                onClick={handleSave}
+              />
+
+              <Button
+                size='small'
+                label='Limpiar'
+                className='bg-bluegray-100 text-gray-900 border-bluegray-100 hover:bg-bluegray-200'
+                type='button'
+                onClick={handleClear}
+              />
+            </div>
           </div>
         </div>
-      </div>
-    </section>
+      </section>
+    </Dialog>
   );
 };
--- client/src/store/denuncias/ratificarDenuncia/ratificarDenuncia.slice.js
+++ client/src/store/denuncias/ratificarDenuncia/ratificarDenuncia.slice.js
@@ -5,7 +5,6 @@ import {
 } from './ratificarDenuncia.thunks';
 
 const initialState = {
-  loading: true,
   resumenDenuncia: null,
   form: {
     firmaDenunciante: null,
@@ -16,14 +15,34 @@ const initialState = {
 export const ratificarDenunciaSlice = createSlice({
   name: 'ratificarDenuncia',
   initialState,
+  reducers: {
+    setFirmaDenunciante: (state, { payload }) => {
+      state.form.firmaDenunciante = payload;
+    },
+    setFirmaFuncionario: (state, { payload }) => {
+      state.form.firmaFuncionario = payload;
+    },
+    clearFirmaDenunciante: (state) => {
+      state.form.firmaDenunciante = null;
+    },
+    clearFirmaFuncionario: (state) => {
+      state.form.firmaFuncionario = null;
+    },
+  },
   extraReducers: (builder) => {
     builder
-      // Ratificar Denuncia
       .addCase(ratificarDenunciaThunk.fulfilled, () => {})
       .addCase(getResumenDenunciaThunk.fulfilled, (state, { payload }) => {
         state.resumenDenuncia = payload;
       });
   },
 });
+
+export const {
+  setFirmaDenunciante,
+  setFirmaFuncionario,
+  clearFirmaDenunciante,
+  clearFirmaFuncionario,
+} = ratificarDenunciaSlice.actions;
 
 export default ratificarDenunciaSlice.reducer;
