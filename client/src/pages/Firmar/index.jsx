1f7aa8b6df56f80a15e9af8dc0e34da7ea8674b0
Mayko2003 <maykox34f@gmail.com>
2023-11-26 17:43:45 -0300

    example firma digital

--- client/src/pages/Firmar/index.jsx
+++ client/src/pages/Firmar/index.jsx
@@ -1,67 +1,100 @@
-import { useEffect, useState } from "react"
+import { useEffect, useState, useRef } from "react"
+import { LacunaWebPKI } from 'web-pki'
 import api from '@/api'
-
+import axios from "axios"
 export const Firmar = () => {
 
-    const [token, setToken] = useState('')
     const [certs, setCerts] = useState(null)
+    const [cert, setCert] = useState('')
+    const [file, setFile] = useState(null)
+    const [token, setToken] = useState('')
+    //create a file ref
+    const fileRef = useRef(null)
 
-    var pki = new LacunaWebPKI('AiEAKi5sYWN1bmFzb2Z0d2FyZS5jb20sanNmaWRkbGUubmV0AAADAFBybwgAAJhSU6eP3AgAAXGQPqewIV4abxw5eh8GZbjYkH5W/JK8lFNbqP0D1hjT5w6Qv3b0Gjb/IrGJ9jjFrB/kVq3StZdY6jXY65iowizIt1Jwhv5RD864TqBMW6IaHAt3YSx7OBJE79PDp1kr8+WmUNkABsqQSPaaLIRsJs5hPx2hvqgmtXTlxWEzaYwLkI+D7mkoKKCIoU3kK+mxA3RXPD27DvckKFeVVQ0aFIF+lnw/Vf++zOtD/5a3SSTZAsEifJfUqauU+FA34MKF/tBnXxhEdv44Kf3aL+cduZlE4dKMmTh9gzaaA1uZiI9O0OTZqYJEocFauDzr8rPAq9UBz1wHfZV8is/iv8oZp5E=');
-
-    const authenticate = async (cert) => {
-        const { data } = await api.get('http://localhost:4000/api/web-pki/authenticationStart', {}, {})
-        setToken(data)
-        pki.signWithRestPki({
-            token: data,
-            thumbprint: cert
-        }).success(async function () {
-	       
-            const { data: data2 } = await api.post('http://localhost:4000/api/web-pki/completeAuthentication', { token: data })
-
-            console.log(data2);
-	    });
-        
-        return 'asdasd'
-    }
+    var pki = new LacunaWebPKI();
 
     useEffect(() => {
 
-        pki.listCertificates().success(function (certs) {
+        pki.listCertificates().success(certs => {
             setCerts(certs)
-            /* for (var i = 0; i < certs.length; i++) {
-                var cert = certs[i];
-                console.log(cert);
-                authenticate().then(res => {
-                    console.log(res);
-                })
-            } */
         });
     }, [])
 
 
     const handleSelect = (e) => {
-        console.log(e.target.value);
-        if (e.target.value)
-            authenticate(e.target.value).then(res => {
+        setCert(e.target.value)
+    }
+
+    const firmar = () => {
+        if(!token || !cert) return;
+        pki.signWithRestPki({
+            token: token,
+            thumbprint: cert
+        }).success(() => {
+            api.post('http://localhost:4000/api/restpki/finish-sign', {token: token}).then(res => {
+                //console.log(res);
                 console.log(res);
             })
+        });
+    }
+
+    
+
+    const reloadCerts = () => {
+        pki.listCertificates().success(function (certs) {
+            setCerts(certs)
+        });
+    }
+
+    const handleSelectFile = (e) => {
+        setFile(e.target.files[0])
+    }
+
+    const startFirma = () => {
+        if (file) {
+            const data = new FormData()
+            data.append('file', file)
+            axios.post('http://localhost:4000/api/restpki/start-sign', data, {
+                headers: {
+                    'Content-Type': 'multipart/form-data'
+                }
+            }).then(res => {
+                console.log(res);
+                setToken(res.data.token)
+            })
+        }
     }
 
     return (
         <div>
-            <h1>HOLA MUNDO</h1>
-            <h1>CERTIFICADOS</h1>
-            <select name="asdasd" id="asdasd" onChange={handleSelect}>
-                <option value={''}>Seleccione un certificado</option>
-                {
-                    certs && certs.map((c, index) => (<option key={index} value={c.thumbprint}>{c.subjectName + ' (issued by ' + c.issuerName + ')'}</option>))
-                }
-            </select>
-            <br />
-            <strong>SERVER RES TOKEN AUTH: </strong><span>{token}</span>
-            <br />
-            {/* <strong>SERVER RES COMPLETE: </strong><span>asdasdasd</span>
- */}
+            <h1>FIRMA DIGITAL</h1>
+            <div style={{ marginTop: '3em' }}>
+                <h1>ARCHIVO A FIRMAR</h1>
+                {/* add use ref */}
+                <input type="file" name="file" id="file" ref={fileRef} onChange={handleSelectFile} />
+            </div>
+            {
+                file && (
+                    <div style={{ marginTop: '3em' }}>
+                        <h1>CERTIFICADOS</h1>
+                        <select name="asdasd" id="asdasd" onChange={handleSelect}>
+                            <option value={''}>Seleccione un certificado</option>
+                            {
+                                certs && certs.map((c, index) => (<option key={index} value={c.thumbprint}>{c.subjectName}</option>))
+                            }
+                        </select>
+                        <br />
+                        <button onClick={reloadCerts}>Recargar Certificados</button>
+                    </div>
+                )
+            }
+            <div style={{ marginTop: '3em' }}>
+                {cert && <button onClick={startFirma}>Comenzar Firma</button>}
+                {token && cert && <button onClick={firmar}>Fimar</button>}
+            </div>
+            <div style={{ marginTop: '3em' }}>
+                <strong>Token: </strong><span>{token}</span>
+            </div>
         </div>
     )
 }
