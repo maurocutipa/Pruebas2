f42617158a380f42d28fcb644c147ef3e58881ab
Mayko2003 <maykox34f@gmail.com>
2023-11-27 10:03:53 -0300

    add storage for restpki

---
+++ server/config/restpki.storage.js
@@ -0,0 +1,21 @@
+const multer = require('multer');
+
+//DISK STORAGE
+const storage = multer.diskStorage({
+    destination: function (req, file, cb) {
+        cb(null, path.join(__dirname, '../uploads/firma-digital'))
+    },
+    filename: function (req, file, cb) {
+        cb(null, file.originalname)
+    }
+})
+
+const uploadFile = multer({ storage: storage }).single('fileFirma')
+
+//MEMORY STORAGE
+const memStorage = multer.memoryStorage()
+
+const uploadFileMem = multer({ storage: memStorage }).single('fileFirma')
+
+
+module.exports = { uploadFile, uploadFileMem }
