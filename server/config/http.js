c3ca0652260901dfcab4120999a9e2c6ecf9faca
mauromamani <mauromamani@gmail.com>
2023-12-01 10:28:51 -0300

    agregar los archivos a la req

--- server/config/http.js
+++ server/config/http.js
@@ -1,7 +1,8 @@
 const axios = require('axios')
 
+const MAYKO_PC = '192.200.0.53'
 const interntalAPI = axios.create({
-    baseURL: `http://${process.env.SERVER_NAME}:3000/api`,
+    baseURL: `http://${MAYKO_PC}:3000/api`,
     headers: {
         Accept: 'application/json',
     }
