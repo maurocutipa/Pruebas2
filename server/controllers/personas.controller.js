27fc70746e311a0e7f8d3aa2b021752f843eba9c
Mayko2003 <maykox34f@gmail.com>
2023-12-05 19:41:52 -0300

    add enviar notificaciones no penal

--- server/controllers/personas.controller.js
+++ server/controllers/personas.controller.js
@@ -46,4 +46,43 @@ PersonasController.getPersonas = async (req, res) => {
   }
 };
 
+PersonasController.enviarNotificaciones = async (req, res) => {
+  try {
+    let { notificaciones } = req.body;
+
+    if (!notificaciones) {
+      throw new Error("Faltan parametros");
+    }
+
+    await Promise.all(notificaciones.map(async noti => {
+      //get id depending of type
+      if(noti.tipo?.toUpperCase() === 'GRUPO'){
+        let query = "SELECT id_notificar_grupos as id FROM notificar_grupos where nombre LIKE ? LIMIT 1";
+        const data = await queryHandler(query, [noti.nombre]);
+        console.log(data);
+        noti.id = data[0]?.id;
+      }
+      else if(noti.tipo?.toUpperCase() === 'PERSONA'){
+        let query = "SELECT id_notificar_grupos_listado as id FROM notificar_grupos_listado WHERE mail LIKE ? LIMIT 1";
+        const data = await queryHandler(query, [noti.mail]);
+        noti.id = data[0]?.id;
+      }
+      else{
+        noti.id = null;
+      }
+
+      let query = "INSERT INTO notificaciones_enviadas(id_tipo, tipo, mail, destinatario,tipo_destinatario, fecha, estado) VALUES (?, ?, ?, ?, ?, NOW(), 1);";
+      await queryHandler(query, [noti.id, noti.tipoNotificacion, noti.mail, noti.nombre, noti.tipo]);
+      return true;
+    }))
+
+    res.status(200).json({
+      ok: true,
+    });
+  } catch (error) {
+    showError(error);
+    httpErrorHandler(res);
+  }
+}
+
 module.exports = PersonasController;
--- server/routes/personas.routes.js
+++ server/routes/personas.routes.js
@@ -6,5 +6,6 @@ const router = Router();
 
 router.post('/get-all', PersonaController.getPersonas)
 router.get('/get-grupos', PersonaController.getGrupos)
+router.post('/enviar-notificaciones', PersonaController.enviarNotificaciones)
 
 module.exports = router
